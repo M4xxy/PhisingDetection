0fbb4be7641cba08954d8c2e29546aca0f8f44ca
Kevin Maxwell Andreas <135214367+M4xxy@users.noreply.github.com>
2024-06-20 16:45:24 +0700

    Add files via upload

---
+++ src/frontend/streamlit_view.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+from src.backend.phising_detector import data_processing, predict_phishing
+
+def phising_url_view(data):
+    
+  # Title and description
+  st.title("Phishing Website Detection App")
+  st.write("Enter a website URL to check if it's likely phishing.")
+
+  # Form elements
+  url = st.text_input("Enter URL:")
+  prediction_select = st.selectbox("Prediction Model:", ("All", "RF", "GBC", "ABC"))
+
+  # Prediction logic based on selection
+  if st.button("Predict"):
+    if url:
+      st.markdown("***Phishing Prediction Result***")
+      processed_data, Scaler = data_processing(data)
+      if prediction_select == "All":
+        model_number = 0
+        for prediction_name in ["RF", "GBC", "ABC"]:
+          prediction = predict_phishing(prediction_name, url, processed_data, Scaler)
+          st.text(f"{prediction_name}: {display_prediction(prediction)}\n")
+          model_number += 1
+      else:
+        prediction = predict_phishing(prediction_select, url, processed_data, Scaler)
+        st.text(f"{prediction_select}: {display_prediction(prediction)}\n")
+        
+def display_prediction(prediction):
+    if prediction == 0:
+        return "Benign!"
+    elif prediction == 1:
+        return "Phishing!"
+    else:
+        return "Incorrect Format!"
